63f7814b539aeb6ec86ad890102a60b957ea2684
FranklySen <alehandro.ribalko@gmail.com>
2024-06-17 22:20:40 +0300

    Add files via upload
    fixes

---
+++ Receiver/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(11000);
+
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+            Console.WriteLine("Waiting for broadcast");
+
+            try
+            {
+                while (true)
+                {
+                    byte[] receiveBytes = udpClient.Receive(ref remoteEndPoint);
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine("Received Message: " + receivedData);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            try
+            {
+                udpClient.Connect("127.0.0.1", 11000);
+
+                Console.WriteLine("Enter the message to send:");
+                string message = Console.ReadLine();
+                byte[] sendBytes = Encoding.ASCII.GetBytes(message);
+
+                udpClient.Send(sendBytes, sendBytes.Length);
+                Console.WriteLine("Message sent to the Receiver.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+        }
+    }
+}
